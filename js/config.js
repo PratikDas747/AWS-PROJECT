36205fa035e46863687d72c7ee5b58dcad603795
PratikDas747 <pratikdas027@gmail.com>
2025-09-29 17:32:49 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_c172eVYv3', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1qldf3sa05gj2b8bdqothtcodg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_MI0DH7sLC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1t6ribglnrrf005dok0dih0tu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
